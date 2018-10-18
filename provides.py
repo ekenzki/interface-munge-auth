393c963fb695567f0a25b814eaba363fb10d1f24
Erik Lönroth <erik.lonroth@gmail.com>
2018-10-18 02:00:13 +0200

    Merging lost changes... (#1)
    * Fixed call to property
    
    * Passing munge key works
    
    * Added munge.configured requirement to avoid publishing on endpoint before key is set
    
    * Change of munge key in provider now gets published to consumer

--- provides.py
+++ provides.py
@@ -1,12 +1,16 @@
 import charms.reactive as reactive
 import charms.reactive.flags as flags
+import charmhelpers.core.hookenv as hookenv
 
 
 class MungeAuthProvides(reactive.Endpoint):
     def expose_munge_key(self, munge_key):
         self._exposed_munge_key = munge_key
+        hookenv.log('expose_munge_key(): exposed in MungeAuthProvides')
 
+    
     @reactive.when('endpoint.{endpoint_name}.changed')
+    @reactive.when('munge.exposed')
     def provide_munge_key(self):
         '''Provide an exposed munge key. Changed flag is set
         when new units are joined as the relation lifecycle
@@ -15,3 +19,4 @@ class MungeAuthProvides(reactive.Endpoint):
             rel.to_publish.update({'munge_key': self._exposed_munge_key})
         flags.clear_flag(self.expand_name(
             'endpoint.{endpoint_name}.changed'))
+        hookenv.log('provide_munge_key(): munge key published on endpoint in MungeAuthProvides')
--- requires.py
+++ requires.py
@@ -4,6 +4,10 @@ import charmhelpers.core.hookenv as hookenv
 
 
 class MungeAuthRequires(reactive.Endpoint):
+
+    def _local_munge_key(self):
+        self._cached_munge_key = hookenv.config().get('munge_key')
+    
     def _munge_auth_relation(self):
         # can only be related to a single munge provider
         assert len(self.relations) < 2
@@ -12,14 +16,15 @@ class MungeAuthRequires(reactive.Endpoint):
     @reactive.when('endpoint.{endpoint_name}.changed.munge_key')
     def check_key(self):
         rel = self._munge_auth_relation()
-        joined_units = rel.joined_units()
+        joined_units = rel.joined_units
         hookenv.log('Joined munge provider units: {}'.format(joined_units))
         remote_unit = hookenv.remote_unit()
         hookenv.log('Remote unit: {}'.format(remote_unit))
-        remote_munge_key = joined_units[remote_unit].get('munge_key')
+        remote_munge_key = joined_units[remote_unit].received.get('munge_key')
         # update a munge key if it's different from a local one
         # there were no per-application endpoint buckets at the time of writing
         # implemented in Juju
+        self._local_munge_key()
         if remote_munge_key and remote_munge_key != self._cached_munge_key:
             self._cached_munge_key = remote_munge_key
             flags.set_flag(self.expand_name(
