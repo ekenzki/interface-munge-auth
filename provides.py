4b5c0455fbfb5c5c4f82bae4c01cd0f983f04385
Erik Lönroth <erik.lonroth@gmail.com>
2018-11-21 22:00:49 +0100

    Munge to make DBD work (#3)
    * Fixed call to property
    
    * Passing munge key works
    
    * Added munge.configured requirement to avoid publishing on endpoint before key is set
    
    * Change of munge key in provider now gets published to consumer
    
    * Takes the munge_key from leadership instead of self._exposed_munge_key in provides.py, as the latter will be undefined when a second consumer joins (i.e. munge.exposed is already set).
    
    TODO: Started writing new_munge_consumer() in provides.py, to only publish the key to new consumers.

--- provides.py
+++ provides.py
@@ -1,6 +1,7 @@
 import charms.reactive as reactive
 import charms.reactive.flags as flags
 import charmhelpers.core.hookenv as hookenv
+import charms.leadership as leadership
 
 
 class MungeAuthProvides(reactive.Endpoint):
@@ -15,8 +16,23 @@ class MungeAuthProvides(reactive.Endpoint):
         '''Provide an exposed munge key. Changed flag is set
         when new units are joined as the relation lifecycle
         includes a joined and consecutive changed event'''
+        hookenv.log('provide_munge_key() triggered, sending out munge key on all relations!!')
         for rel in self.relations:
-            rel.to_publish.update({'munge_key': self._exposed_munge_key})
+            #rel.to_publish.update({'munge_key': self._exposed_munge_key})
+            rel.to_publish.update({'munge_key': leadership.leader_get('munge_key')})
         flags.clear_flag(self.expand_name(
             'endpoint.{endpoint_name}.changed'))
         hookenv.log('provide_munge_key(): munge key published on endpoint in MungeAuthProvides')
+
+    # TODO: Should not be done in the interface?
+    # Should we distinguish between providing munge keys to all relations (as the function above)
+    # or just the currently joined unit?
+    @reactive.when('endpoint.{endpoint_name}.changed')
+    @reactive.when('leadership.set.munge_key')
+    def new_munge_consumer(self):
+        remote_unit = hookenv.remote_unit()
+        if remote_unit:
+            mk = leadership.leader_get('munge_key')
+            hookenv.log('new_munge_consumer(): join event from %s, publishing key: %s' % (remote_unit, mk))
+            #rel = self.relation[remote_unit]
+            #rel.to_publish.update({'munge_key': mk})
